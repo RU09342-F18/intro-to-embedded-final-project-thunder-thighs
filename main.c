91522a155a77079aeee33424a6ba382f5040ad05
NickSetaro <33162219+NickSetaro@users.noreply.github.com>
2018-12-09 20:54:35 -0500

    Update main.c

--- main.c
+++ main.c
@@ -4,22 +4,26 @@
 /**
  * main.c
  */
-float time, adjustedTime;
+float time, distance;
+
 void initializeGPIO () {
-    P1DIR |= BIT2;      //pin 1.2 trigger
-    P6DIR |= BIT6;      //pin 6.0 echo
+    P1DIR |= BIT0;      //led test pin
+    P1DIR |= BIT3;      //pin 1.3 echo
+    P2DIR |= BIT0;      //pin 2.0 trigger
+    P1IFG &= ~BIT3;     //clear interrupt flag
+    P1IE |= BIT3;       //enable port interrupt
+    P1IES &= ~BIT3;     //set interrupt edge low/high
 }
 
 void initializeTimerSend() {
-    TA0CTL = TASSEL_2 + MC_1 + ID_0 + TACLR;
-    TA0CCR0 = 0;
-    TA0CCTL1 |= OUTMOD_3;
+    TA0CTL = TASSEL_2 + MC_1 + TACLR;       //SMCLK in up mode
+    TA0CCR0 = 20000;           //send every 20000us
+    TA0CCTL0 = CCIE;        //enable interrupt
 }
 
 void initializeTimerT() {
-    TA1CTL = TASSEL_1 + MC_1 + ID_0 + TACLR;
-    TA1CCR0 = 0;
-    TA1CCTL1 |= OUTMOD_3;
+    TA1CTL = TASSEL_2 + MC_0  + TACLR;      //SMCLK in stop mode
+    TA1CCR0 = 50000;
 }
 
 void initializeUART() {
@@ -39,22 +43,35 @@ void initializeUART() {
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	void initializeGPIO();
-	void initializeTimer();
-	void initializeUART();
-	
-	return 0;
+	initializeGPIO();
+	initializeTimerSend();
+	initializeUART();
+	initializeTimerT();
+	__bis_SR_register(GIE);
+	while(1);
 }
 
-#pragma vector = PORT6_VECTOR
-__interrupt void Port_6(void){
-    TA1CCTL = MC_0;         //stop timer
+#pragma vector = PORT1_VECTOR
+__interrupt void Port_1(void){
+
+    TA1CTL = MC_0;          //stop timing timer
     time = TA1R;            //get time value
     distance = time/58;     //convert to distance
+    TA1CTL = TACLR;         //clear timing timer
+    TA0CTL = MC_1;          //start send timer
+    P1IFG &= ~BIT3;         //clear interrupt flag
+    P1OUT &= ~BIT3;         //set trigger low
+
 }
 
-#pragma vector = TIMER0_A1_VECTOR
-__interrupt void Timer_Send (void) {
-    P1DIR ^= BIT2;          //send trigger
-    TA1CCTL = MC_1;         //start timer
+#pragma vector = TIMER0_A0_VECTOR           //send timer
+__interrupt void Timer_Send (void){
+
+    P2OUT |= BIT0;          //send trigger
+    TA0CTL = MC_0;          //stop send timer
+    TA0CTL = TACLR;         //reset send timer
+    TA0IV = 0;              //clear send timer interrupt flag
+    TA1CTL = MC_2;          //start timing timer in continuous mode
+
 }
+
